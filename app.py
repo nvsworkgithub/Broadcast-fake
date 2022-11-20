746df807f0751082f3a15d73ed8a6f2db0581130
CODE-BUDDY <111114081+BUDDY1609@users.noreply.github.com>
2022-11-20 23:15:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Join Telegram Channal:- @tgnvs'
+    return 'join Telegra, Channal'
+
+
+if __name__ == "__main__":
+    app.run()
