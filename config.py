720bbace0bfdba047c064a97c9f0addc1cb4c2ff
CODE-BUDDY <111114081+BUDDY1609@users.noreply.github.com>
2022-12-06 00:25:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-DB_URL = os.environ.get("DB_URL", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5539701008:AAHT5Vl8kIG_iGalo8F1Pj-QsPgVk_-eJEc")
+API_ID = int(os.environ.get("API_ID", "12124605"))
+API_HASH = os.environ.get("API_HASH", "5cf3577d85fd02286535ec2296934287")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001546645785"))
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5490240193").split())
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://BUDDY:BUDDY@cluster0.fntfd.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
